b65ab5c3a0c9596be960079f10bfbb096fc78550
jose.silveira3 <jose.silveira3@LAB9-25-WIN.acad.univates>
2023-08-14 20:35:35 -0300

    Fizemos uma tela que pega informação de um campo de texto, e ao clicar no botão, exibe-a em um alerta.

---
+++ Aula3/src/aula3/Aula3.java
@@ -0,0 +1,18 @@
+
+package aula3;
+
+/**
+ *
+ * @author jose.silveira3
+ */
+public class Aula3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        
+    }
+    
+}
